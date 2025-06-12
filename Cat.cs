e4488b7b9682fb5b7226958ec677406e3830db17
lee sua <s2332@e-mirim.hs.kr>
2025-06-12 14:57:14 +0900

    고양이 Eat, Sleep 메서드 하이딩

--- Cat.cs
+++ Cat.cs
@@ -8,6 +8,8 @@ namespace PolyExample
 {
     class Cat :Animal
     {
+        public new void Eat() { Console.WriteLine("냐미냐미 예쁘게 먹습니다."); }
+        public new void Sleep() { Console.WriteLine("소근소근 잠을 잡니다."); }
         public void Meow() { Console.WriteLine("냥냥 웁니다."); }
 
     }
