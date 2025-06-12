abdab3d950c30561053a63643bafef2b2b158e57
lee sua <s2332@e-mirim.hs.kr>
2025-06-12 15:04:07 +0900

    강아지 Eat, Sleep 메서드 오버라이딩

--- Animal.cs
+++ Animal.cs
@@ -22,7 +22,7 @@ namespace PolyExample
 
         public int Age { get; set; }
 
-        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
-        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+        public virtual void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+        public virtual void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
     }
 }
